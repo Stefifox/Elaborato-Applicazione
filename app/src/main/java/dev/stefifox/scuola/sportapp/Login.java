62407e6dc8248d9bf06000f1d8e9ec9c53d74d37
Stefano <30950154+Stefifox@users.noreply.github.com>
2021-05-21 15:38:01 +0200

    Corretto login, rimosso CallApi.java

--- app/src/main/java/dev/stefifox/scuola/sportapp/Login.java
+++ app/src/main/java/dev/stefifox/scuola/sportapp/Login.java
@@ -1,5 +1,8 @@
 package dev.stefifox.scuola.sportapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,44 +12,102 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
     //Dichiaro le variabili
     private String username;
     private String password;
-
+    private boolean success = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        final HashMap<String, String>[] userdata = new HashMap[]{new HashMap<>()};
+        final HashMap<String, String> userdata = new HashMap<>();
 
         //Dichiaro i Widget
         final Button login = findViewById(R.id.login_button);
         final EditText mailText = findViewById(R.id.mail);
         final EditText passText = findViewById(R.id.pass);
 
-        final CallApi api = new CallApi();
         //Dichiaro il funzionamemto dei pulsanti
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 username = mailText.getText().toString();
                 password = passText.getText().toString();
-                userdata[0] = api.loginFuction(username, password, Login.this);
 
-                if(!userdata[0].containsKey("error")){
+                //Parametri della richiesta
+                String loginUrl = MainActivity.serverUrl + "/login";
+                Map<String, String> params = new HashMap<>();
 
-                }else{
+                params.put("token", "51c8b422852557a12d3778270037538c");
+                params.put("mail", username);
+                params.put("password", password);
+                JSONObject parameters = new JSONObject(params);
 
-                }
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, loginUrl, parameters, new Response.Listener<JSONObject>() {
 
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject obj = new JSONObject(response.getJSONObject("user").toString()); //Genero un oggetto dal risultato
+                            save("id", obj.getInt("id"));
+                            save("nome", obj.getString("nome"));
+                            save("cognome", obj.getString("cognome"));
+                            save("mail", obj.getString("mail"));
+                            save("username", obj.getString("username"));
+                            save("token", obj.getString("identityToken"));
+                            System.out.println(obj.toString()); //Print di debug
+                            callMain(true); //Avvio la pagina principale una volta ottenute le informazioni
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Login.this, "Errore di autentucazione, riprova", Toast.LENGTH_SHORT).show(); //In caso di errore
+                    }
+                });
+                //Chiamo l'istanza per eseguire la richiesta
+                MySingleton.getInstance(Login.this).addToRequestQueue(request);
             }
         });
 
+    }
+
+    private void callMain(boolean result){
+        if(result){
+            Intent main = new Intent(Login.this, MainActivity.class);
+            startActivity(main); //Avvio l'activity main
+            finish(); //Termino l'activity login
+        }
+    }
+
+    private void save (String data, String value){
+        SharedPreferences save = getSharedPreferences(data, MODE_PRIVATE);
+        SharedPreferences.Editor saveEdit = save.edit();
+        saveEdit.putString(data + "_", value);
+        saveEdit.apply();
+    }
+
+    private void save (String data, int value){
+        SharedPreferences save = getSharedPreferences(data, MODE_PRIVATE);
+        SharedPreferences.Editor saveEdit = save.edit();
+        saveEdit.putInt(data + "_", value);
+        saveEdit.apply();
     }
 
 }
--- app/src/main/java/dev/stefifox/scuola/sportapp/MainActivity.java
+++ app/src/main/java/dev/stefifox/scuola/sportapp/MainActivity.java
@@ -2,13 +2,113 @@ package dev.stefifox.scuola.sportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.VoiceInteractor;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String serverUrl = "http://192.168.86.24:5500";
+
+    private String token = "n";
+    private int id;
+    private String nome;
+    private String cognome;
+    private String username;
+    private String mail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView userShow = findViewById(R.id.username);
+        final TextView mailShow = findViewById(R.id.mail_text);
+
+        token = loadToken();
+        id = loadId();
+
+        if(token.equals(getToken(String.valueOf(id)))){
+
+        }
+
+        nome = loadName();
+        cognome = loadCognome();
+        username = loadUsername();
+        mail = loadMail();
+
+        userShow.setText(username);
+        mailShow.setText(mail);
+
     }
+
+    private int loadId(){
+        SharedPreferences load = getSharedPreferences("id", MODE_PRIVATE);
+        int id = load.getInt("id_", this.id);
+        return id;
+    }
+
+    private String loadToken(){
+        SharedPreferences load = getSharedPreferences("token", MODE_PRIVATE);
+        String token = load.getString("token_", this.token);
+        return token;
+    }
+
+    private String loadName(){
+        SharedPreferences load = getSharedPreferences("nome", MODE_PRIVATE);
+        String nome = load.getString("nome_", this.nome);
+        return nome;
+    }
+
+    private String loadCognome(){
+        SharedPreferences load = getSharedPreferences("cognome", MODE_PRIVATE);
+        String cognome = load.getString("cognome_", this.cognome);
+        return cognome;
+    }
+
+    private String loadUsername(){
+        SharedPreferences load = getSharedPreferences("username", MODE_PRIVATE);
+        String username = load.getString("username_", this.username);
+        return username;
+    }
+
+    private String loadMail(){
+        SharedPreferences load = getSharedPreferences("mail", MODE_PRIVATE);
+        String mail = load.getString("mail_", this.mail);
+        return mail;
+    }
+
+
+    private String getToken(String id){
+        String getUrl = serverUrl + "/getToken?id=" + id;
+        final String[] responseString = new String[1];
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, getUrl, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    responseString[0] = response.getJSONObject("token").toString();
+                } catch (JSONException e) {
+                    responseString[0] = "null";
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        return responseString[0];
+    }
+
 }
